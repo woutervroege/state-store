18a16256b9fe6b427dd4e59461cc2450ebd77c46
Wouter Vroege <w.vroege@vpro.nl>
2022-02-21 22:05:49 +0100

    edited demo db

--- demo/db.js
+++ demo/db.js
@@ -1,9 +1,8 @@
 import { db } from '../src/LocalStorage.js';
 
-db.validate('name', (name) => {
-  const nameHasCaps = name.match(/[A-Z]/);
-  if(!nameHasCaps) return true;
-  console.warn('Value cannot have caps, please lowercase...');
+db.validate('name', name => {
+  if( name.match(/.{3,}/) ) return true;
+  console.warn('A name should at least have 3 characters.');
   return false;
 })
 
